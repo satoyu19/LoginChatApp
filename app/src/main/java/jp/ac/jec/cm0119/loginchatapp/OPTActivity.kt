094de13a566e086ff77257f8bef84f56c34a5ab3
satoyu <yuuutaas124@gmail.com>
2022-11-08 10:01:08 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/jec/cm0119/loginchatapp/OPTActivity.kt
@@ -0,0 +1,11 @@
+package jp.ac.jec.cm0119.loginchatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OPTActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_optactivity)
+    }
+}
---
+++ app/src/main/java/jp/ac/jec/cm0119/loginchatapp/VerificationActivity.kt
@@ -0,0 +1,16 @@
+package jp.ac.jec.cm0119.loginchatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * ログイン、サインイン
+ * * **/
+class VerificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification)
+    }
+
+
+}
